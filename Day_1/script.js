4e4002061cc67dfcc50b09850907a9a932ef28d2
Jay van Wyk <jayvanwyk@gmail.com>
2022-09-21 17:51:42 +0100

    Added background image file

--- Day_1/script.js
+++ Day_1/script.js
@@ -1,7 +1,20 @@
-window.addEventListener('keydown', (e)=>{
-        const audio = document.querySelector(`audio[data-key="${e.key.toUpperCase()}"]`);
-    console.log(audio);
-    if(audio){
-        audio.play();
-    }
+window.addEventListener('keydown', (e) => {
+  const audio = document.querySelector(
+    `audio[data-key="${e.key.toUpperCase()}"]`
+  );
+  const key = document.querySelector(`div[data-key="${e.key.toUpperCase()}"]`);
+  key.classList.add('playing');
+
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+});
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key=>{
+    key.addEventListener('transitionend', (e)=>{
+        if(e.propertyName !== 'transform') return;
+        console.log(this);
+        this.classList.remove('playing');
+    });
 })
